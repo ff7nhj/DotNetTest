3c56082c981c568439eed0e5018b6ef4c42a36ab
Mumus <mm@majnik.hu>
2018-09-25 21:16:03 +0200

    First Unit Test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,10 +8,21 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        const string MyName = "Marci";
         static void Main(string[] args)
         {
-            const string myName = "Marci";
-            Console.WriteLine("Hello {0}!",myName);
+            HelloWorld();
+            
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello {0}!", MyName);
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
         }
     }
 }
